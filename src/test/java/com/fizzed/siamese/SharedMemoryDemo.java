76fa6a7f828f724445562a20638c71f476afefcb
Joe Lauer <joe@lauer.bz>
2023-10-10 22:49:53 -0400

    Rename project

--- src/test/java/com/fizzed/siamese/SharedMemoryDemo.java
+++ src/test/java/com/fizzed/siamese/SharedMemoryDemo.java
@@ -1,8 +1,11 @@
 package com.fizzed.siamese;
 
+import com.fizzed.jne.JNE;
+import com.fizzed.jne.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,7 +14,9 @@ public class SharedMemoryDemo {
     static private final Logger log = LoggerFactory.getLogger(SharedMemoryDemo.class);
 
     static public void main(String[] args) throws Exception {
-        Path libFile = Paths.get("native/target/debug/libjsiamese.so");
+        Options options = new Options();
+        String libraryName = options.createLibraryName("jsiamese", options.getOperatingSystem(), null, null, null);
+        Path libFile = Paths.get("native/target/debug", libraryName);
         String libPath = libFile.toAbsolutePath().toString();
 
         log.debug("Loading lib {}", libPath);
