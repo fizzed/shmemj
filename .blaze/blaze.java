525aa10cfb65c1c7cc01b5048c7f389d38fcfd10
Joe Lauer <joe@lauer.bz>
2023-10-20 00:01:33 -0400

    MacOS lib built on 10.13 instead of 12

--- .blaze/blaze.java
+++ .blaze/blaze.java
@@ -26,7 +26,7 @@ public class blaze {
     final private Logger log = Contexts.logger();
 
     private final List<Target> targets = asList(
-        // Linux arm64 (ubuntu 18.04, glibc 2.27+)
+        /*// Linux arm64 (ubuntu 18.04, glibc 2.27+)
         new Target("linux", "x64")
             .setTags("build")
             .setContainerImage("fizzed/buildx:amd64-ubuntu18-jdk11-cross-build"),
@@ -60,12 +60,14 @@ public class blaze {
         new Target("linux", "riscv64")
             .setTags("build")
             .setContainerImage("fizzed/buildx:amd64-ubuntu18-jdk11-cross-build"),
+        */
 
         // MacOS x64 (10.13+)
         new Target("macos", "x64")
             .setTags("build", "test")
             .setHost("bmh-build-x64-macos1013-1"),
 
+        /*
         // MacOS arm64 (12+)
         new Target("macos", "arm64")
             .setTags("build", "test")
@@ -79,13 +81,13 @@ public class blaze {
         // Windows arm64 (win10+)
         new Target("windows", "arm64")
             .setTags("build")
-            .setHost("bmh-build-x64-win11-1"),
+            .setHost("bmh-build-x64-win11-1"),*/
 
         //
         // test-only containers
         //
 
-        new Target("linux", "x64-test")
+        /*new Target("linux", "x64-test")
             .setTags("test")
             .setContainerImage("fizzed/buildx:amd64-ubuntu18-jdk11"),
 
@@ -117,7 +119,7 @@ public class blaze {
         new Target("linux_musl", "arm64-test")
             .setTags("test")
             //.setHost("bmh-build-arm64-ubuntu22-1")
-            .setContainerImage("fizzed/buildx:arm64v8-alpine3.11-jdk11"),
+            .setContainerImage("fizzed/buildx:arm64v8-alpine3.11-jdk11"),*/
 
         new Target("windows", "x64-test", "win10")
             .setTags("test")
@@ -125,11 +127,11 @@ public class blaze {
 
         new Target("windows", "x64-test", "win7")
             .setTags("test")
-            .setHost("bmh-build-x64-win7-1"),
+            .setHost("bmh-build-x64-win7-1")
 
-        new Target("windows", "arm64-test", "win11")
+        /*new Target("windows", "arm64-test", "win11")
             .setTags("test")
-            .setHost("bmh-build-arm64-win11-1")
+            .setHost("bmh-build-arm64-win11-1")*/
     );
 
     public void build_containers() throws Exception {
